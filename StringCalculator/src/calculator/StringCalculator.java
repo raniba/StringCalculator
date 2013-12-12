7cbc537a0935fa734d540cde13d334486564b4e3
raniba <ranibarghouti@yahoo.com>
2013-12-12 18:47:54 +0200

    Blue: (REFACTOR) add stToInt private function to convert string to int

--- StringCalculator/src/calculator/StringCalculator.java
+++ StringCalculator/src/calculator/StringCalculator.java
@@ -2,14 +2,19 @@
 package calculator;
 
 public class StringCalculator {
+	private int stToInt(String string)//convert String to int
+	{
+		return Integer.parseInt(string);
+	}
+	
 	public int Add(String string)
 	{
 		if(string.isEmpty())
 			return 0;
 		String[] numbers = string.split(",");
 		 if(numbers.length == 1)
-			 return Integer.parseInt(string);
-		return Integer.parseInt(numbers[0])+Integer.parseInt(numbers[1]);
+			 return stToInt(string);
+		return stToInt(numbers[0])+stToInt(numbers[1]);
 	}
 
 }
