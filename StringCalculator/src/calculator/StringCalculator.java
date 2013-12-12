aab5bab507b6b803df3a0670e3d13bf800892c70
raniba <ranibarghouti@yahoo.com>
2013-12-12 17:57:26 +0200

    RED: addingEmptyStringReturns0 function-test fails

--- StringCalculator/src/calculator/StringCalculator.java
+++ StringCalculator/src/calculator/StringCalculator.java
@@ -1,5 +1,10 @@
+//raniba
 package calculator;
 
 public class StringCalculator {
+	public int Add(String string)
+	{
+		return -1;
+	}
 
 }
--- StringCalculator/src/calculator/StringCalculatorTest.java
+++ StringCalculator/src/calculator/StringCalculatorTest.java
@@ -1,3 +1,4 @@
+//raniba
 package calculator;
 
 import static org.junit.Assert.*;
@@ -8,7 +9,20 @@ public class StringCalculatorTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
+	}
+	
+	@Test
+	public void addingEmptyStringReturns0() //input: empty string, output:zero
+	{
+		//arrange
+		StringCalculator calculator = new StringCalculator();
+	
+		//act
+		int result= calculator.Add("");
+		
+		//assert
+		assertEquals(0,result);
 	}
 
 }
